81885b8f3397c9bee8b93f28f9ba6f0a5bd443bd
David Haugen <haugen.dj@gmail.com>
2019-02-22 18:33:33 -0600

    prep for heroku deployment

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const cors = require('cors');
 const helmet = require('helmet');
 const POKEDEX = require('./pokedex.json');
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
 app.use(helmet());
 app.use(cors());
 
 app.use(function validateBearerToken(req, res, next) {
   const authToken = req.get('Authorization');
   const apiToken = process.env.API_TOKEN;
-  console.log('validate bearer token middleware');
   // move to the next middleware
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' });
@@ -24,7 +24,6 @@ app.use(function validateBearerToken(req, res, next) {
 });
 
 
-console.log(process.env.API_TOKEN);
 
 const validTypes = ['Bug', 'Dark', 'Dragon', 'Electric', 'Fairy', 'Fighting', 'Fire', 'Flying', 'Ghost', 'Grass', 'Ground', 'Ice', 'Normal', 'Poison', 'Psychic', 'Rock', 'Steel', 'Water'];
 
@@ -51,9 +50,17 @@ function handleGetPokemon(req, res) {
 app.get('/pokemon', handleGetPokemon);
 
 
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }};
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
 });
