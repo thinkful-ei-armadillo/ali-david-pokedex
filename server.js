c8b347363e64f5f329024d569ab919c1d66224a5
Ali Lahrime <alahrime@email.wm.edu>
2019-02-24 18:59:01 -0500

    heroku deploy changes

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const morgan = require('morgan')
 const app = express()
 const POKEDEX = require('./pokedex.json')
 
-app.use(morgan('dev'))
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
+app.use(morgan(morganSetting))
 
 app.use(function validateBearerToken(req, res, next) {
   const authToken = req.get('Authorization')
   const apiToken = process.env.API_TOKEN
-  console.log('validate bearer token middleware')
   // move to the next middleware
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' })
@@ -18,7 +18,6 @@ app.use(function validateBearerToken(req, res, next) {
 })
 
 
-console.log(process.env.API_TOKEN)
 
 const validTypes = [`Bug`, `Dark`, `Dragon`, `Electric`, `Fairy`, `Fighting`, `Fire`, `Flying`, `Ghost`, `Grass`, `Ground`, `Ice`, `Normal`, `Poison`, `Psychic`, `Rock`, `Steel`, `Water`]
 
@@ -29,10 +28,10 @@ function handleGetTypes(req, res) {
 app.get('/types', handleGetTypes)
 
 function handleGetPokemon(req, res) {
-  const  name = req.query.name
-  const  type = req.query.type
+  const name = req.query.name
+  const type = req.query.type
   let results = POKEDEX.pokemon
-  
+
   if (req.query.name) {
   results = results.filter((item) => item.name.toLowerCase() === name.toLowerCase()) 
 }
@@ -44,10 +43,16 @@ function handleGetPokemon(req, res) {
 
 app.get('/pokemon', handleGetPokemon)
 
-
-
-const PORT = 8000
-
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`)
+app.use((error, req, res, next) => {
+  let response
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error }
+  }
+  res.status(500).json(response)
 })
+
+const PORT = process.env.PORT || 8000
+
+app.listen(PORT)
